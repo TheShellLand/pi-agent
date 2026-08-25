295fbd204c7d233cab13a66b46c5f7b97126ae34
naisanzaa <naisanza@gmail.com>
2026-08-25 16:04:59 +0700

    Add test.sh script to run the agent in a Docker container

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# run agent in docker
+
+image="pi-agent"
+mount="."
+folder=$(basename "`pwd`" | sed 's/ /-/g')
+
+set -e
+
+docker network create agents >/dev/null 2>&1 || :
+
+set -x
+
+docker run --rm -it \
+  --network agents \
+  --shm-size=2g \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  -v $image-sessions:/root/.pi/agent/sessions \
+  -v $image-ssh:/root/.ssh \
+  -v $mount:/root/brain \
+  $image -v 
+
